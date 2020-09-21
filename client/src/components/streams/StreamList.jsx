dbeeb013184f45b2581817e779b95c5f669eebc5
marenda <markostevic96@hotmail.com>
2020-09-21 17:29:11 +0200

    Delete Stream

--- client/src/components/streams/StreamList.jsx
+++ client/src/components/streams/StreamList.jsx
@@ -13,7 +13,7 @@ class StreamList extends React.Component{
             return(
                 <div className="right floated content">
                     <Link to={`/streams/edit/${stream.id}`} className="ui button primary" >Edit</Link>
-                    <button className="ui button negative">Delete</button>
+                    <Link className="ui button negative" to={`/streams/delete/${stream.id}`} >Delete</Link>
                 </div>
             );
         }
